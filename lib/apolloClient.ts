a5f5bb801e2295a44079b810ec237f851987edec
Mateusz Jonak <mateusz.jonak@gmail.com>
2020-08-19 17:14:02 +0200

    Setup contentful

---
+++ lib/apolloClient.ts
@@ -0,0 +1,11 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } = process.env;
+
+export const client = new ApolloClient({
+  uri: `https://graphql.contentful.com/content/v1/spaces/${CONTENTFUL_SPACE_ID}`,
+  cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Bearer ${CONTENTFUL_ACCESS_TOKEN}`,
+  },
+});
