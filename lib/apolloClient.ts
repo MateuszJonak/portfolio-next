8c5c53ae4d023daa93223b5abf657946e0e96b84
Mateusz Jonak <mateusz.jonak@gmail.com>
2020-08-19 18:27:35 +0200

    Stop apollo caching

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -4,7 +4,9 @@ const { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } = process.env;
 
 export const client = new ApolloClient({
   uri: `https://graphql.contentful.com/content/v1/spaces/${CONTENTFUL_SPACE_ID}`,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    resultCaching: false,
+  }),
   headers: {
     Authorization: `Bearer ${CONTENTFUL_ACCESS_TOKEN}`,
   },
