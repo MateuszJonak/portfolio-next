63dbe66c57172c90f0909efea85fd8b51ab337c2
Mateusz Jonak <mateusz.jonak@gmail.com>
2020-08-19 18:44:24 +0200

    Disable cache on apollo

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,6 +1,17 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, DefaultOptions } from '@apollo/client';
 
 const { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } = process.env;
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+};
 
 export const client = new ApolloClient({
   uri: `https://graphql.contentful.com/content/v1/spaces/${CONTENTFUL_SPACE_ID}`,
@@ -10,4 +21,5 @@ export const client = new ApolloClient({
   headers: {
     Authorization: `Bearer ${CONTENTFUL_ACCESS_TOKEN}`,
   },
+  defaultOptions,
 });
