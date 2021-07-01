25125335ec1eb71b808d6715e50409f5087ae7f0
Mateusz Jonak <mateusz.jonak@gmail.com>
2021-07-01 15:00:01 +0200

    fix: preconnect is now auto added by next.js

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,7 +29,6 @@ export default class MyDocument extends Document {
           />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700&display=swap"
